3d82c3f479a82e8c5491055b784d90529d919ebe
liuzewei <670115782@qq.com>
2020-05-07 18:49:04 +0800

    修改配置

--- blog-web/src/test/java/com/blog/framework/BasicGenerateApplicationTests.java
+++ blog-web/src/test/java/com/blog/framework/BasicGenerateApplicationTests.java
@@ -1,16 +1,5 @@
 package com.blog.framework;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class BasicGenerateApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
 
 }
